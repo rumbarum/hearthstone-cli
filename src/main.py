d4497f80567e9cf40901cad257e3a5acbff5668b
Bareum Han <rumbarum@gmail.com>
2022-12-31 15:24:23 +0900

    initial setting

--- src/main.py
+++
@@ -1,5 +0,0 @@
-def main() -> None:
-    print("하스스톤에 오신걸 환영합니다.")
-
-if __name__ == "__main__":
-    main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-from src.main import main
+from stone.main import main
 
 def test_main():
     main()
