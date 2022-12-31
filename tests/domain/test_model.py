a85be618c55d98bab8546c5cc2d92dab6f2936d8
Bareum Han <rumbarum@gmail.com>
2022-12-31 19:08:23 +0900

    test(tests/): Attack Command Test

---
+++ tests/domain/test_model.py
@@ -0,0 +1,34 @@
+import pytest
+
+from stone.domain.command import Attack
+from stone.domain.model import BattleField, Player
+from stone.service_layer.handler import handle_message
+
+
+@pytest.fixture
+def players():
+    player1 = Player()
+    player2 = Player()
+    return player1, player2
+
+
+@pytest.fixture
+def battle_field(players):
+    field = BattleField(players=list(players))
+    return field
+
+
+def test_attack_player(battle_field):
+    player_a, player_b = battle_field.players
+    attack_player = Attack(
+        attack=1, target=player_a.uuid, source=player_b.uuid
+    )
+    battle_field.message_slot.append(attack_player)
+    battle_field.message_slot.append(attack_player)
+    battle_field.message_slot.append(attack_player)
+    battle_field.message_slot.append(attack_player)
+    battle_field.message_slot.append(attack_player)
+
+    handle_message(field=battle_field)
+
+    player_a.life == 25
