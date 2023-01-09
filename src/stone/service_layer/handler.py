f52341fb9c1502442417a4dcd5e7adbab4eba656
Bareum Han <rumbarum@gmail.com>
2023-01-09 22:22:23 +0900

    feat: add melee_attack_handler

--- src/stone/service_layer/handler.py
+++ src/stone/service_layer/handler.py
@@ -1,15 +1,19 @@
-from stone.domain.command import Attack
+import typer
+
+from stone.domain import command, event, model
 from stone.domain.model import BattleField
 
 
-def handle_attack(comm: Attack, field: BattleField):
-    target = field.get_player_by_uuid(comm.target)
-    target.life -= comm.attack
+def handle_melee_attack(command: command.MeleeAttack, field: BattleField):
+    target = field.get_target_by_uuid(command.target)
+    attacker = field.get_target_by_uuid(command.source)
+    target.life -= command.attack
+    attacker.life -= target.attack
 
 
-def handle_message(field: BattleField):
-    while slot := field.message_slot:
-        message = slot.popleft()
-        if isinstance(message, Attack):
-            handle_attack(message, field)
-            continue
+def after_attacked(event: event.Attacked, field: BattleField):
+    pass
+
+
+COMMAND_HANDLERS = {command.MeleeAttack: handle_melee_attack}
+EVENT_HANDLERS = {event.Attacked: [after_attacked]}
