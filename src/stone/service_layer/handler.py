5d3fe104ea49b0bf1d237108a3fe1b3ef0b733bd
Bareum Han <rumbarum@gmail.com>
2023-01-10 21:29:02 +0900

    feat(handler.py): add ranged attack handler

--- src/stone/service_layer/handler.py
+++ src/stone/service_layer/handler.py
@@ -11,9 +11,17 @@ def handle_melee_attack(command: command.MeleeAttack, field: BattleField):
     attacker.life -= target.attack
 
 
+def handle_ranged_attack(command: command.RangedAttack, field: BattleField):
+    target = field.get_target_by_uuid(command.target)
+    target.life -= command.attack
+
+
 def after_attacked(event: event.Attacked, field: BattleField):
     pass
 
 
-COMMAND_HANDLERS = {command.MeleeAttack: handle_melee_attack}
+COMMAND_HANDLERS = {
+    command.MeleeAttack: handle_melee_attack,
+    command.RangedAttack: handle_ranged_attack,
+}
 EVENT_HANDLERS = {event.Attacked: [after_attacked]}
