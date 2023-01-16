ca30ccc9e740569bd1ef0658c3ce8c5ef5b43db6
Bareum Han <rumbarum@gmail.com>
2023-01-16 22:22:46 +0900

    feat: add Attacked event and handler

--- tests/domain/test_model.py
+++ tests/domain/test_model.py
@@ -1,7 +1,7 @@
 import pytest
 
 from stone.bootstrap import bootstrap
-from stone.domain import command, event
+from stone.domain import commands, events
 from stone.domain.model import BattleField, Minion, Player
 
 
@@ -38,7 +38,7 @@ def test_minion_melee_attack_player_with_damage_1_decrease_1_life(
 ):
     pl1, pl2 = battle_field.players.values()
     min1 = pl1.minion_field[0]
-    melee_attack = command.MeleeAttack(
+    melee_attack = commands.MeleeAttack(
         source=min1.uuid, target=pl2.uuid, attack=min1.attack
     )
     message_bus.handle(melee_attack)
@@ -49,11 +49,45 @@ def test_minion_melee_attack_player_with_damage_1_decrease_1_life(
 def test_player_ranged_attack_minon_with_damage_50_decrease_50_life(
     battle_field, message_bus
 ):
+    """
+    given
+        2 player with 1 minion each
+    when
+        pl2 attack pl1's minion
+    then
+        minion1 life decrease
+        player1 life stay
+    """
     pl1, pl2 = battle_field.players.values()
     min1 = pl1.minion_field[0]
-    ranged_attack = command.RangedAttack(
+    ranged_attack = commands.RangedAttack(
         source=pl2.uuid, target=min1.uuid, attack=50
     )
     message_bus.handle(ranged_attack)
     assert min1.life == 50
     assert pl2.life == 30
+
+
+def test_display_console_minion_attack_other_minion(
+    battle_field, message_bus, capsys
+):
+    """
+    given
+        2 player with 1 minion each
+    when
+        pl1 minion attack pl2 minion
+    then
+        display 2 message will show up
+    """
+    pl1, pl2 = battle_field.players.values()
+    min1 = pl1.minion_field[0]
+    min2 = pl2.minion_field[0]
+    melee_attack = commands.MeleeAttack(
+        source=min1.uuid, target=min2.uuid, attack=min1.attack
+    )
+    message_bus.handle(melee_attack)
+    captured = capsys.readouterr()
+    assert (
+        f"""{min1.uuid[:5]} damaged {min2.uuid[:5]} by {min1.attack:3}\n{min2.uuid[:5]} damaged {min1.uuid[:5]} by {min2.attack:3}\n"""
+        == captured.out
+    )
