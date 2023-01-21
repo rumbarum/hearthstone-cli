9bfa5df8b69f953d2b14b731e30bef5c638b7cc1
Bareum Han <rumbarum@gmail.com>
2023-01-21 18:00:12 +0900

    feat: card related method

--- src/stone/domain/model.py
+++ src/stone/domain/model.py
@@ -13,7 +13,7 @@ from stone.domain import commands, events
 class Spell:
     name: str
     attack: int
-    uuid: str = field(default_factory=lambda: str(uuid4()))
+    uuid: str = field(default_factory=lambda: str(uuid4()), kw_only=True)
 
 
 @dataclass(kw_only=True)
@@ -21,23 +21,35 @@ class Minion:
     name: str
     attack: int
     life: int
-    uuid: str = field(default_factory=lambda: str(uuid4()))
+    uuid: str = field(default_factory=lambda: str(uuid4()), kw_only=True)
+
+
+CardObject = Union[Spell | Minion]
 
 
 @dataclass(kw_only=True)
 class Card:
     mana: int
-    object: Union[Spell | Minion]
+    object: CardObject
+    uuid: str = field(default_factory=lambda: str(uuid4()), kw_only=True)
 
 
 @dataclass
 class Player:
-    card_slot: list = field(default_factory=list)
-    minion_field: list[Minion] = field(default_factory=list)
+    card_slot: list[Card] = field(default_factory=list)
+    minion_field: list[Minion] = field(
+        default_factory=lambda: list([None] * 7)
+    )
     uuid: str = field(default_factory=lambda: str(uuid4()))
     life: int = 30
     mana: int = 0
     attack: int = 0
+
+    def get_card_from_player(self, card_uuid: str):
+        for card in self.card_slot:
+            if card_uuid == card.uuid:
+                return card
+        raise ValueError("NO_MATCHING_CARD")
 
 
 @dataclass
@@ -66,7 +78,7 @@ class BattleField:
         target_min = None
         for player in self.players.values():
             for minion in player.minion_field:
-                if minion.uuid == uuid:
+                if minion is not None and minion.uuid == uuid:
                     target_min = minion
         if target_min:
             return target_min
@@ -76,3 +88,8 @@ class BattleField:
     def collect_new_messages(self) -> Generator:
         while slot := self.message_slot:
             yield slot.popleft()
+
+    def get_card_from_player(self, player_uuid: str, card_uuid: str):
+        player = self.get_player_by_uuid(player_uuid)
+        card = player.get_card_from_player(card_uuid)
+        return card
